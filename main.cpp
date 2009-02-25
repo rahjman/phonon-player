3a1b1eb19a24ddcd80366a205619e23dbd8ce30d
rahjman <rahman.duran@gmail.com>
2009-02-25 22:00:57 +0200

    Added audio playback support using Phonon::MediaObject. Source file given via command line parameter.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QtCore/QCoreApplication>
+#include <phonon/mediaobject.h>
+#include <phonon/audiooutput.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    a.setApplicationName("phonon-player");
+
+    Phonon::MediaObject *mediaObject = new Phonon::MediaObject( &a );
+    Phonon::AudioOutput *audioOutPut = new Phonon::AudioOutput(Phonon::MusicCategory, &a);
+    Phonon::createPath(mediaObject, audioOutPut);
+
+    mediaObject->setCurrentSource(Phonon::MediaSource(argv[1]));
+
+    mediaObject->play();
 
     return a.exec();
 }
